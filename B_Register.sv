4a0952cd6223aa405f0087c7904cc5b76df5246d
Mohammed-Soliman <s-mohammed.soliman@zewailcity.edu.eg>
2025-12-04 15:02:17 +0200

    Add files via upload

---
+++ B_Register.sv
@@ -0,0 +1,13 @@
+module b (
+    input  logic        clk, reset,
+    input  logic        b_write,
+    input  logic [7:0]  b_in,
+    output logic [7:0]  b_out
+);
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            b_out <= 8'h00;
+        else if (b_write)
+            b_out <= b_in;
+    end
+endmodule
---
+++ Top_Module.sv
@@ -0,0 +1,11 @@
+module arm1_top (
+    input  logic clk,
+    input  logic reset
+);
+    
+    arm1_datapath processor (
+        .clk(clk),
+        .reset(reset)
+    );
+    
+endmodule
